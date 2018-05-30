3c01be6efd9ebeaa08e4f95b4dab5d4ccfe98cd1
SMARTBIRD123 <1925788413@qq.com>
2018-05-30 13:06:58 +0800

    第一次上传特效

---
+++ js/data.js
@@ -0,0 +1,25 @@
+
+/*
+*   type :  0 -- DIV  1--JS  2--H5  3--Css3
+*
+*
+* */
+var flyData = [
+    {
+        type : "666",
+        author : "羊习习",
+        title : "我感觉你是在为难我胖虎",
+        dec : "这就很凶了",
+        time : "xxxx-xx-zz",
+        src : "3D Drag"
+    },
+    {
+        type : "Js",
+        author : "羊习习",
+        title : "我感觉你是在为难我胖虎",
+        dec : "这就很凶了",
+        time : "xxxx-xx-xx",
+        src : "3D Drag"
+    }
+];
+
